85f453e5df2139468397bad98dd6204065433872
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-04-13 13:38:28 +0300

    Added value in cents for Coin

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,21 @@ enum Coin {
     Eur2,
 }
 
+impl Coin {
+    fn value_in_cents(&self) -> u32 {
+        match self {
+            Coin::Cent1 => 1,
+            Coin::Cent2 => 2,
+            Coin::Cent5 => 5,
+            Coin::Cent10 => 10,
+            Coin::Cent20 => 20,
+            Coin::Cent50 => 50,
+            Coin::Eur1 => 100,
+            Coin::Eur2 => 200 
+        }    
+    }
+}
+
 fn main() {
     let amount = Amount { euros: 2, cents: 57 };
     println!("{}", amount);
