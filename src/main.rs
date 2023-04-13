ad688ae7255f3aa7903a5128ca35e00ac2aba1e4
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-04-13 13:19:43 +0300

    Added impl for Amount

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,22 @@ struct Amount {
     cents: u32, 
 }
 
+impl Amount {
+    fn as_cents(&self) -> u32 {
+        self.euros * 100 + self.cents
+    }
+}
+
 impl fmt::Display for Amount {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}.{:02} \u{20AC}", self.euros, self.cents)
     }
 }
 
-fn value_in_cents(amount: &Amount) -> u32 {
-    amount.euros * 100 + amount.cents
-}
-
 fn main() {
     let amount = Amount { euros: 2, cents: 57 };
     println!("{}", amount);
-    println!("In cents = {}", value_in_cents(&amount));
+    println!("In cents = {}", amount.as_cents());
 
     println!("The amount is still {:?}", amount);
 }
