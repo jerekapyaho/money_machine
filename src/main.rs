dea8e310a9510dc2480a8dfc7b760f90c577a140
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-04-13 12:54:59 +0300

    Use reference

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,14 @@ struct Amount {
     cents: u32, 
 }
 
+fn value_in_cents(amount: &Amount) -> u32 {
+    amount.euros * 100 + amount.cents
+}
+
 fn main() {
     let amount = Amount { euros: 2, cents: 57 };
     println!("{}.{:02} \u{20AC}", amount.euros, amount.cents);
+    println!("In cents = {}", value_in_cents(&amount));
+
+    println!("The amount is still {:?}", amount);
 }
