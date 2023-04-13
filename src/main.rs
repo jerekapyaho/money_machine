f103a1c0906e09a8c8f172400191389660b92b32
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-04-13 13:28:42 +0300

    Added constructor for Amount

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,29 @@ struct Amount {
     cents: u32, 
 }
 
-impl Amount {
-    fn as_cents(&self) -> u32 {
-        self.euros * 100 + self.cents
-    }
-}
-
 impl fmt::Display for Amount {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}.{:02} \u{20AC}", self.euros, self.cents)
     }
 }
 
+impl Amount {
+    fn new(cents: u32) -> Self {
+        Amount {
+            euros: cents / 100,
+            cents: cents % 100
+        }
+    }
+    
+    fn as_cents(&self) -> u32 {
+        self.euros * 100 + self.cents
+    }
+}
+
 fn main() {
     let amount = Amount { euros: 2, cents: 57 };
     println!("{}", amount);
-    println!("In cents = {}", amount.as_cents());
 
-    println!("The amount is still {:?}", amount);
+    let new_amount = Amount::new(586);
+    println!("{}", new_amount);
 }
