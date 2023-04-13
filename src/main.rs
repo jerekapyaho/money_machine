d865ac6db0272931d318188d4331f2dd4633c980
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-04-13 13:13:09 +0300

    Added Display trait implementation for Amount

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use std::fmt;
+
 /// Represents a monetary amount in euros and cents.
 #[derive(Debug)]
 struct Amount {
     euros: u32,
     cents: u32, 
+}
+
+impl fmt::Display for Amount {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}.{:02} \u{20AC}", self.euros, self.cents)
+    }
 }
 
 fn value_in_cents(amount: &Amount) -> u32 {
@@ -11,7 +19,7 @@ fn value_in_cents(amount: &Amount) -> u32 {
 
 fn main() {
     let amount = Amount { euros: 2, cents: 57 };
-    println!("{}.{:02} \u{20AC}", amount.euros, amount.cents);
+    println!("{}", amount);
     println!("In cents = {}", value_in_cents(&amount));
 
     println!("The amount is still {:?}", amount);
